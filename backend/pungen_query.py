68297e9f44b90ba6a4ed2c19bee4ddc87ae2a0b5
Quansheng Tan <qtan4@illinois.edu>
2015-03-21 02:40:57 -0500

    consolidated code

--- backend/pungen_query.py
+++ backend/pungen_query.py
@@ -1,5 +1,8 @@
 import urllib2
 from bs4 import BeautifulSoup as bs
+import json
+from nltk.probability import FreqDist as FD
+from nltk.book import text9
 
 # Query punGenerator
 def generatePuns(word):
@@ -11,13 +14,50 @@ def generatePuns(word):
 
 	# Process HTML response
 	soup = bs(html)
-	puns = [s for s in soup.find_all("td")]
+	temp = [s for s in soup.find_all("td")]
 
-	for p in puns:
+        # Extract puns
+        puns = []
+	for p in temp:
 		pw = p.contents[-1].strip()
 		if pw:
-			print pw
+			puns.append(pw)
+        return puns
+
+# Query Reddit
+def queryReddit():
+	
+	# HTML query
+	o = urllib2.build_opener()
+	html = o.open("http://www.reddit.com/r/jokes/top.json?sort=top&t=all&limit=100&count=0").read()
+	js = json.loads(html)["data"]["children"]
+
+	# Process HTML response
+	total = ""
+	for p in js:
+		total += p["data"]["selftext"]
+
+	# Return
+	return total
+
+# Tokenize English
+def preproc(arg):
+    freq = FD(arg)
+    freq_lower = dict()
+    total = 0
+
+    for tup in freq:
+        freq_lower[tup.lower()] = freq_lower.get(tup.lower(), 0 ) + freq[tup];
+        total += freq[tup]
+
+    return dict((k, v/total) for (k,v) in freq_lower)
+
+def preprocEnglish():
+    return preproc(text9)
+
+def preprocReddit():
+    return preproc(queryReddit())
 
 # Test
 if __name__ == "__main__":
-	generatePuns("muck")
+	print preprocReddit()
