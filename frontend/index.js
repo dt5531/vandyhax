1cb2a6d36d7106999108be8b94f3411e8c77d459
Laura Licari <lalicari@comcast.net>
2015-03-21 11:35:32 -0500

    update front end

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,34 @@
 $(document).ready(function(){
     $("#toggle").click(function(){
-        $("#hide").slideToggle("slow");
+        $("#hide").slideDown("slow");
+        $("#search").val("");
+    });
+});
+
+$(document).ready(function(){
+	$(".clickable").click(function(){//click on the suggested search
+		$("#hide").slideDown("slow");//show the search box
+		$("#search").val($(this).text());//set the text in the search box
+		$("#table").slideDown("slow");
+		if( ($("#search").val()).length >= 3 ){
+    		$("#table").show();
+    		//$("#search").show();
+    	}
+    	else{
+    		$("#search").show();
+    		$("#table").hide();
+    	}
+	});
+});
+$(document).ready(function(){
+	$("#hide").on("keyup keypress keydown click", function(){
+    	if( ($("#search").val()).length >= 3 ){
+    		$("#table").show();
+    		//$("#search").show();
+    	}
+    	else{
+    		$("#search").show();
+    		$("#table").hide();
+    	}
     });
 });
