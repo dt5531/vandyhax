d0adaeaf7c7b97ca50af0843f5bb7d730c5aff37
Laura Licari <clarecurtis@Clare-Curtiss-MacBook-Pro.local>
2015-03-21 19:39:20 -0500

    background

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $("#toggle").click(function(){
+    	$("#search").val("");
+    	$("#toggle").hide();
+    	$("#table").hide();
         $("#hide").slideDown("slow");
-        $("#search").val("");
-        $("#table").hide();
         $("#search").focus();
     });
 });
@@ -10,11 +11,11 @@ $(document).ready(function(){
 $(document).ready(function(){
 	$(".clickable").click(function(){//click on the suggested search
 		$("#hide").slideDown("slow");//show the search box
+		$("#toggle").slideUp("slow");
 		$("#search").val($(this).text());//set the text in the search box
 		$("#table").slideDown("slow");
 		if( ($("#search").val()).length >= 3 ){
     		$("#table").show();
-    		//$("#search").show();
     	}
     	else{
     		$("#search").show();
@@ -26,11 +27,33 @@ $(document).ready(function(){
 	$("#hide").on("keyup keypress keydown click", function(){
     	if( ($("#search").val()).length >= 3 ){
     		$("#table").show();
-    		//$("#search").show();
     	}
     	else{
     		$("#search").show();
     		$("#table").hide();
     	}
     });
+});
+
+$(document).ready(function(){
+	var w = $(window).innerWidth();
+    var h = $(window).innerHeight();
+	$(".background").each(function(){
+		var tempSize = Math.random()*2.5;
+		var t = Math.random() * h*0.70;
+	    var r = Math.random() * w*0.70;
+	    var newBlur = w/50*tempSize;
+	    var newBlurStr = newBlur.toString() + "px";
+		$(this).rotate(Math.random()*360);
+	    $(this).css({
+	    	"top": t.toString()+"px",
+	    	"right": r.toString()+"px",
+	    	"position": "fixed",
+	    	"width": (w/8*tempSize).toString()+"px",
+	    	"height": (w/8*tempSize).toString()+"px",
+	    	"background-size": "100%, 100%",
+	    	"filter": "blur(" + newBlurStr + ")",
+	    	"-webkit-filter": "blur(" + newBlurStr + ")",
+	    });
+	});
 });
